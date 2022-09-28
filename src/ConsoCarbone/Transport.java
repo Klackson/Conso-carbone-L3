6b0ae6c561777e00002571615407a07054951221
Cazou21 <114527465+Cazou21@users.noreply.github.com>
2022-09-28 22:34:27 +0200

    Update Transport.java

--- src/ConsoCarbone/Transport.java
+++ src/ConsoCarbone/Transport.java
@@ -11,8 +11,16 @@ public class Transport extends consoCarbone{
      this.taille=P;
      this.kilomAnnee=0;
      this.amortsissement=0;
+   }
      
+   public Transport(boolean possede, Taille taille, int kilomAnnee,int amortissement){
+     this.possede=possede;
+     this.taille=taille;
+     this.kilomAnnee=kilomAnnee;
+     this.amortissement=amortissement;
      
+      
+       
       
        
        
