bc4011767d4c6d5c983862d81f1ecd84e3f9155e
Cazou21 <114527465+Cazou21@users.noreply.github.com>
2022-12-02 11:34:09 +0100

    Update exemple.java

--- src/Utilisateur/exemple.java
+++ src/Utilisateur/exemple.java
@@ -10,7 +10,7 @@ import ConsoCarbone.BienConso;
 public class exemple {
 	public static void main(String[] args) {
 		Logement rg60 = new Logement(65,CE.A);
-		Transport voituremelvin = new Transport(false, Taille.G, 200, 10, 4.5);
+		Transport voituremelvin = new Transport(false, Taille.G, 200, 10, 4.5,25);
 		Alimentation repasmelvin = new Alimentation(0,1);
 		BienConso thunemelvin = new BienConso(5000);
 
